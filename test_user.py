aaed3876fbd15a196c772d70faf4c3a0224427f7
Fredrik Kock <freko247@gmail.com>
2014-04-01 23:33:23 +0200

    Implemented test functions in generice test case class.

--- test_user.py
+++ test_user.py
@@ -5,20 +5,9 @@ from google.appengine.api import memcache
 from google.appengine.ext import testbed
 from google.appengine.ext import ndb
 
-import db_functions
+import models
 
-
-def testAddUser():
-    userData = {
-        'userName': 'John Doe',
-        'userEmail': 'john@mail.com',
-        # userSkill: ['Python', 'Mobile', 'Java'],
-        # userTag: ['DTU', 'Digital Media Engineering', 'Android'],
-    }
-    return db_functions.addUser(userData)
-
-
-class DemoTestCase(unittest.TestCase):
+class GenericTestCase(unittest.TestCase):
     def setUp(self):
         # First, create an instance of the Testbed class.
         self.testbed = testbed.Testbed()
@@ -31,6 +20,40 @@ class DemoTestCase(unittest.TestCase):
     def tearDown(self):
         self.testbed.deactivate()
 
-    def testInsertEntity(self):
-        userKey = testAddUser()
+    def testAddUser(self):
+        userEmail = 'user@mail.com'
+        userName = 'John Doe'
+        userSkill = ['Python', 'Communication', 'Leadership']
+        userTag = ['DTU', 'Digital Media Engineering', 'Android']
+        user = models.User(key=ndb.Key('User', userEmail))
+        user.populate(userEmail=userEmail,
+                      userName=userName,
+                      userSkill=userSkill,
+                      userTag=userTag,
+                      )
+        userKey = user.put()
         self.assertTrue(isinstance(userKey, ndb.Key))
+
+    def testAddUserRating(self):
+        # Add user
+        userEmail = 'test.user@mail.com'
+        userName = 'John Doe'
+        user = models.User(key=ndb.Key('User', userEmail),
+                           userEmail=userEmail,
+                           userName=userName)
+        user.put()
+        ratingUserEmail = 'rating.user@mail.com'
+        ratingUserName = 'Jane Doe'
+        ratingUser = models.User(key=ndb.Key(models.User,
+                                             ratingUserEmail),
+                                 userEmail=ratingUserEmail,
+                                 userName=ratingUserName)
+        ratingUser.put()
+        # Add rating
+        rating = models.Rating(key=ndb.Key(models.User,
+                                           user.userEmail,
+                                           models.Rating,
+                                           ratingUser.userEmail),
+                               ratingUser=ratingUser.userEmail,
+                               ratingValue=5).put()
+        self.assertTrue(isinstance(rating, ndb.Key))
