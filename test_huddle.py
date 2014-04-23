96b8ce6e47e64e504ca92ad777b10df32aad5301
Fredrik Kock <freko247@gmail.com>
2014-04-23 13:31:15 +0200

    Removed unnecesssary imports.

--- test_huddle.py
+++ test_huddle.py
@@ -1,14 +1,10 @@
 # -*- coding:utf8 -*-
-import unittest
-from datetime import datetime
-
-from google.appengine.ext import testbed
 from google.appengine.ext import ndb
 
 import baseTest
 import db_functions
-import models
 from fixtures import huddleFixtures
+
 
 class HuddleTestCase(baseTest.GenericTestCase):
     def testCreateHuddle(self):
