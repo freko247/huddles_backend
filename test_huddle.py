a06f399c24ec3c584b2611a577e5e5e78ffd0484
Fredrik Kock <freko247@gmail.com>
2014-05-01 22:25:46 +0200

    Updated tests to fit to updated getHuddleInfo and getSuggestedHuddles.

--- test_huddle.py
+++ test_huddle.py
@@ -1,7 +1,10 @@
 # -*- coding:utf8 -*-
 import baseTest
+from datetime import datetime
 import copy
-import db_functions, document_functions
+import logging
+
+import db_functions
 from fixtures import huddleFixtures, settingsFixtures
 
 
@@ -13,8 +16,13 @@ class HuddleTestCase(baseTest.GenericTestCase):
 
     def testGetHuddleInfo(self):
         huddleName = db_functions.createHuddle(huddleFixtures[0])
-        huddleInfo = db_functions.getHuddleInfo({'huddleName': huddleName})
-        self.assertEqual(huddleFixtures[0]['huddleTag'], huddleInfo)
+        huddle = db_functions.getHuddleInfo({'huddleName': huddleName})
+        huddlesTimestamp = datetime.utcfromtimestamp(
+            int(huddleFixtures[0]['huddleDateAndTime'][:-3]))
+        self.assertEqual(huddleFixtures[0]['huddleName'], huddle[0])
+        self.assertEqual(huddleFixtures[0]['huddleTag'], huddle[1])
+        self.assertEqual(huddleFixtures[0]['huddleLocation'], huddle[2])
+        self.assertEqual(huddlesTimestamp, huddle[3])
 
     def testJoinHuddle(self):
         huddleData = copy.deepcopy(huddleFixtures[0])
@@ -32,5 +40,14 @@ class HuddleTestCase(baseTest.GenericTestCase):
 
     def testGetHuddlesInRange(self):
         db_functions.createHuddle(huddleFixtures[0])
-        document_functions.getHuddlesInRange(settingsFixtures)
-        self.assertEqual(1, 1)
+        huddles = db_functions.getSuggestedHuddles(settingsFixtures)
+        logging.debug([str(huddleFixtures[0]['huddleName']),
+                       huddleFixtures[0]['huddleTag'],
+                       huddleFixtures[0]['huddleLocation'],
+                       huddleFixtures[0]['huddleDateAndTime']])
+        huddlesTimestamp = datetime.utcfromtimestamp(
+            int(huddleFixtures[0]['huddleDateAndTime'][:-3]))
+        self.assertEqual(huddleFixtures[0]['huddleName'], huddles[0][0])
+        self.assertEqual(huddleFixtures[0]['huddleTag'], huddles[0][1])
+        self.assertEqual(huddleFixtures[0]['huddleLocation'], huddles[0][2])
+        self.assertEqual(huddlesTimestamp, huddles[0][3])
